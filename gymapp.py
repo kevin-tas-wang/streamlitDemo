60ca6102d3c26b5e0a77d18cce531df5deecbeaa
kevin-tas-wang <58488929+kevin-tas-wang@users.noreply.github.com>
2023-09-13 23:07:09 +1200

    Update gymapp.py

--- gymapp.py
+++ gymapp.py
@@ -9,3 +9,14 @@ st.write(
 st.sidebar.write("## Upload a sample data  :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload a sample file", type=["csv"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload a file smaller than 5MB.")
+    else:
+        st.write("You have uploaded successfully!")
+else:
+    st.write("Try uploading something :grin:")
