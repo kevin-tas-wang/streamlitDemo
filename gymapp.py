411a0f91caa59a1a7efdfbe59459bdc3b1b8b226
kevin-tas-wang <58488929+kevin-tas-wang@users.noreply.github.com>
2023-09-13 12:11:35 +0000

    update mainfile

--- gymapp.py
+++ gymapp.py
@@ -20,11 +20,11 @@ from xgboost import XGBClassifier
 
 st.set_page_config(layout="wide", page_title="Gym Injurey Prediction Testing")
 
-st.write("## Find the right model for Gym Injurey Prediction")
+st.write("### Find the right model for Gym Injurey Prediction")
 st.write(
-    ":dog: Try uploading some test data to explore different models /settings in prediction :grin:"
+    ":dog: Try uploading some test data to explore different models/settings in prediction"
 )
-st.sidebar.write("## Upload a sample data  :gear:")
+st.sidebar.write("## Upload a sample data")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
